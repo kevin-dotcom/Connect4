5e93e83583667faa60d54b69a1cfd35af707132a
Kevin Zhang <2581490265a@gmail.com>
2020-05-23 16:43:47 -0400

    Setup a basic board system.

--- src/game/Test.java
+++ src/game/Test.java
@@ -1,13 +1,9 @@
 package game;
 
-import ai.Computer;
-
 public class Test {
 	
 	public static void main(String[] args) {
-		Computer c = new Computer(Computer.HARD);
 		
-		System.out.println(c.getDifficulty());
 	}
 	
 }
